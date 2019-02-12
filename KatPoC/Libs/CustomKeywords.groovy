fd9b677c94f94549f18466840e80c0884ce91713
dmanager002 <dmanager002@MRICZHALOAPS004.us.nam.ad.pwcinternal.com>
2019-02-11 23:39:01 -0500

    added test case and keyword

--- KatPoC/Libs/CustomKeywords.groovy
+++ KatPoC/Libs/CustomKeywords.groovy
@@ -3,3 +3,25 @@
  * This class is generated automatically by Katalon Studio and should not be modified or deleted.
  */
 
+import com.kms.katalon.core.testobject.TestObject
+
+import java.lang.String
+
+
+def static "com.pwc.CustomKeywords.refreshBrowser"() {
+    (new com.pwc.CustomKeywords()).refreshBrowser()
+}
+
+def static "com.pwc.CustomKeywords.clickElement"(
+    	TestObject to	) {
+    (new com.pwc.CustomKeywords()).clickElement(
+        	to)
+}
+
+def static "com.pwc.CustomKeywords.getHtmlTableRows"(
+    	TestObject table	
+     , 	String outerTagName	) {
+    (new com.pwc.CustomKeywords()).getHtmlTableRows(
+        	table
+         , 	outerTagName)
+}
